403b4a1dfd794ebe4a2794d0399e434958d458c7
Guilherme Raposo <gui.raposo36@gmail.com>
2022-04-02 12:39:00 -0300

    feat: created state in react'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import { Tweet } from "./components/Tweet";
 
 function App() {
+  const [tweets, setTweets] = useState<string[]>([
+    "Tweet 1",
+    "Tweet 2",
+    "Tweet 3",
+    "Tweet 4",
+  ]);
+
+  function createTweet() {
+    setTweets([...tweets, "Tweet 5"]);
+  }
+
   return (
     <div>
-      <Tweet text="Tweet 1" />
-      <Tweet text="Tweet 2" />
-      <Tweet text="Tweet 3" />
+      {tweets.map((tweet) => {
+        return <Tweet key={tweet} text={tweet} />;
+      })}
+
+      <button onClick={createTweet}>Adicionar Tweet</button>
     </div>
   );
 }
